7b802f8f6aba6ed35ed72899e7f8a51dd3476157
Roman Lavrenov <lavr001@gmail.com>
2021-02-18 13:52:04 -0800

    header is complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="header">
+          <div className="logo_box">
+            <div className="logo"></div>
+          </div>
+          <div className="tabs_box">
+            <div className="tab_name">Products</div>
+            <div className="tab_name">Resources</div>
+            <div className="tab_name">Support</div>
+            <div className="tab_name">Events</div>
+            <div className="tab_name">Developer</div>
+          </div>
+          <div className="contact_box">
+            <div className="accounts">
+              View Accounts
+            </div>
+            <div className="contact">
+              Contact Sales
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
